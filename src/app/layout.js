2cb4076b9be582fe4a18b3b08410de8355d5fd33
hyu <i3lxyu@gmail.com>
2025-04-18 00:17:04 +0800

    +

---
+++ src/app/layout.js
@@ -0,0 +1,96 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Image from "next/image";
+import AvatarImg from "@/../public/Avatar.png";
+import Link from "next/link";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "我的數位履歷",
+  description: "還沒做好QAQ",
+
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        
+        <div className="w-screen h-screen overflow-y-auto flex flex-col sm:flex-row gap-5 bg-gray-200 p-8">
+
+          <div className=" min-w-[320px] w-full sm:w-[320px] h-full rounded-2xl bg-white flex 
+          justify-start items-center flex-col p-[30px] pt-[80px] overflow-y-auto overflow-x-hidden">
+            
+            <Image src={AvatarImg} alt="Avatar" className="w-[80px] h-[80px] rounded-full" />
+        
+            <h1 className="mt-2">曾泓語</h1>
+            <p className="max-w-[380px] text-center">
+              網頁趕工中...
+            </p>
+            
+            {/* 社群連結 */}
+            <div className="flex gap-2 my-5">
+              <a href="https://google.com" target="_blank">
+                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
+              </a>
+              <a href="https://google.com" target="_blank">
+                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
+              </a>
+              <a href="https://google.com" target="_blank">
+                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
+              </a>
+              <a href="https://google.com" target="_blank">
+                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
+              </a>
+              <a href="https://google.com" target="_blank">
+                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
+              </a>
+            </div>
+
+            {/* 頁面切換 */}
+            <div className="w-full flex flex-col gap-2">
+              
+              <Link href="/about">
+                {/* <div className="w-full h-[84px] bg-gray-300 rounded-xl">About</div> */}
+                <div className={`w-full h-[84px] bg-gray-300 rounded-xl`}>About</div>
+              </Link>
+
+              <Link href="/cate-web">
+                <div className="w-full h-[84px] bg-gray-300 rounded-xl">Cate-Web</div>
+              </Link>
+
+              <Link href="/cate-painting">
+                <div className="w-full h-[84px] bg-gray-300 rounded-xl">Cate-Painting</div>
+              </Link>
+              
+              
+              <div className="w-full h-[84px] bg-gray-300 rounded-xl">作品分類三</div>
+              <div className="w-full h-[84px] bg-gray-300 rounded-xl">作品分類一</div>
+              <div className="w-full h-[84px] bg-gray-300 rounded-xl">作品分類二</div>
+              <div className="w-full h-[84px] bg-gray-300 rounded-xl">作品分類三</div>
+            </div>
+
+          </div>
+
+          {children}
+
+        </div>
+
+      </body>
+    </html>
+  );
+}
