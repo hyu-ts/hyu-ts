fe167a7055ccb2d3640fbb3a77a6f78b3701b946
hyu <i3lxyu@gmail.com>
2025-04-18 22:36:05 +0800

    ~

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import AvatarImg from "@/../public/Avatar.png";
+import FbImg from "@/../public/facebook.png";
+import InstImg from "@/../public/instagram.png";
+import ThreadsImg from "@/../public/threads.png";
 import Link from "next/link";
 
 const geistSans = Geist({
@@ -17,7 +20,6 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "我的數位履歷",
   description: "還沒做好QAQ",
-
 };
 
 export default function RootLayout({ children }) {
@@ -29,67 +31,64 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
         <div className="w-screen h-screen overflow-y-auto flex flex-col sm:flex-row gap-5 bg-gray-200 p-8">
+          <div
+            className="min-w-[320px] w-full sm:w-[320px] h-full rounded-2xl bg-white flex 
+            justify-start items-center flex-col p-[30px] pt-[80px] overflow-y-auto overflow-x-hidden"
+          >
+            <Image
+              src={AvatarImg}
+              alt="Avatar"
+              className="w-[80px] h-[80px] rounded-full"
+            />
 
-          <div className=" min-w-[320px] w-full sm:w-[320px] h-full rounded-2xl bg-white flex 
-          justify-start items-center flex-col p-[30px] pt-[80px] overflow-y-auto overflow-x-hidden">
-            
-            <Image src={AvatarImg} alt="Avatar" className="w-[80px] h-[80px] rounded-full" />
-        
-            <h1 className="mt-2">曾泓語</h1>
-            <p className="max-w-[380px] text-center">
-              網頁趕工中...
+            <h1 className="mt-2 text-xl font-semibold">曾泓語</h1>
+            <p className="max-w-[300px] text-center text-sm text-gray-600">
+              20y | NCCU
             </p>
-            
+
             {/* 社群連結 */}
-            <div className="flex gap-2 my-5">
-              <a href="https://google.com" target="_blank">
-                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
+            <div className="flex gap-4 my-5">
+              <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                <Image src={FbImg} alt="Facebook" width={30} height={30} className="cursor-pointer" />
               </a>
-              <a href="https://google.com" target="_blank">
-                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
+
+              <a href="https://www.instagram.com/hongyu_ts" target="_blank" rel="noopener noreferrer">
+                <Image src={InstImg} alt="Instagram" width={30} height={30} className="cursor-pointer" />
               </a>
-              <a href="https://google.com" target="_blank">
-                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
+
+              <a href="https://www.threads.net/hongyu_ts" target="_blank" rel="noopener noreferrer">
+                <Image src={ThreadsImg} alt="Threads" width={30} height={30} className="cursor-pointer" />
               </a>
-              <a href="https://google.com" target="_blank">
-                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
-              </a>
-              <a href="https://google.com" target="_blank">
-                <div className="bg-gray-300 w-[40px] h-[40px] rounded-full"></div>
-              </a>
+
+              
             </div>
 
             {/* 頁面切換 */}
             <div className="w-full flex flex-col gap-2">
-              
               <Link href="/about">
-                {/* <div className="w-full h-[84px] bg-gray-300 rounded-xl">About</div> */}
-                <div className={`w-full h-[84px] bg-gray-300 rounded-xl`}>About</div>
+                <div className={`w-full h-[84px] bg-gray-300 rounded-xl flex justify-center items-center`}>
+                  About
+                </div>
               </Link>
 
-              <Link href="/cate-web">
-                <div className="w-full h-[84px] bg-gray-300 rounded-xl">Cate-Web</div>
-              </Link>
-
-              <Link href="/cate-painting">
-                <div className="w-full h-[84px] bg-gray-300 rounded-xl">Cate-Painting</div>
-              </Link>
-              
-              
-              <div className="w-full h-[84px] bg-gray-300 rounded-xl">作品分類三</div>
-              <div className="w-full h-[84px] bg-gray-300 rounded-xl">作品分類一</div>
-              <div className="w-full h-[84px] bg-gray-300 rounded-xl">作品分類二</div>
-              <div className="w-full h-[84px] bg-gray-300 rounded-xl">作品分類三</div>
+              <div className="w-full h-[84px] bg-gray-300 rounded-xl flex justify-center items-center">
+                （空）
+              </div>
+              <div className="w-full h-[84px] bg-gray-300 rounded-xl flex justify-center items-center">
+                （空）
+              </div>
+              <div className="w-full h-[84px] bg-gray-300 rounded-xl flex justify-center items-center">
+                （如也）
+              </div>
+              <div className="w-full h-[84px] bg-gray-300 rounded-xl flex justify-center items-center">
+                以後做個小遊戲放這
+              </div>
             </div>
-
           </div>
 
           {children}
-
         </div>
-
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,40 @@
-import React from "react";
-import Link from "next/link";
+"use client"
 
-export default function Layout({ children }) {
+import { useState } from "react";
+import BlurText from "../component/BlurText";
+
+export default function Home() {
+
+  const [house, setHouse] = useState(0);
+
+  const addHouse = function() {
+    setHouse(house + 1);
+  }
+
+  const handleAnimationComplete = () => {
+    console.log('Animation completed!');
+  };
+
   return (
-    <div>
-      <header>
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-        </nav>
-      </header>
-      <main>{children}</main>
-      <footer>
-        <p>© 2025 Your Website</p>
-      </footer>
+    <div className="flex w-full h-full bg-red justify-center items-center rounded-2xl">
+      
+      <BlurText
+        text="救救孩子！"
+        delay={150}
+        animateBy="words"
+        direction="top"
+        onAnimationComplete={handleAnimationComplete}
+        className="text-2xl mb-8"
+      />
+
+      <div className="cursor-pointer flex flex-col justify-center items-center"
+        onClick={addHouse}
+      >
+        <div className="text-6xl">{'🏚️'}</div>  {/* Broken house emoji */}
+        <div className="text-2xl">{house}</div>
+      </div>
+      
     </div>
   );
 }
+
