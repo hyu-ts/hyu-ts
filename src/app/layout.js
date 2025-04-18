b2cf47445ca442e59102528577198814342bbfb7
hyu <i3lxyu@gmail.com>
2025-04-19 02:21:35 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,7 +44,7 @@ export default function RootLayout({ children }) {
 
             <h1 className="mt-2 text-xl font-semibold">曾泓語</h1>
             <p className="max-w-[300px] text-center text-sm text-gray-600">
-              哈囉！這是我的網站。
+              實名上網。
             </p>
 
             {/* 社群連結 */}
