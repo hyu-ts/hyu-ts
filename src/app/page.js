f88ad47e515f9b5928f051f4f79854af8520b3a0
hyu <i3lxyu@gmail.com>
2025-04-18 22:59:20 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import Image from "next/image";
-import AvatarImg from "@/../public/Avatar.png";
+"use client"
 
-export default function About() {
+import { useState } from "react";
+
+export default function Home() {
   return (
     <div className="w-full h-full bg-white flex justify-center items-center rounded-2xl">
       
-      <p>HOME</p>
+      <p>ABOUT</p>
     </div>
   );
 }
