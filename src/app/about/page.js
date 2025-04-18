057b4e8cc9823e1e2a3da825981f53c5c1cce91e
hyu <i3lxyu@gmail.com>
2025-04-19 00:00:54 +0800

    ~

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import AvatarImg from "@/../public/Avatar.png";
-
 export default function About() {
   return (
     <div className="w-full h-full bg-white flex justify-center items-center rounded-2xl">
