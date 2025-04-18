e8e09353ba946f77d6effa1f2c5f9db4ecdd78b1
hyu <i3lxyu@gmail.com>
2025-04-18 22:57:01 +0800

    -

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,11 @@
-"use client"
+import Image from "next/image";
+import AvatarImg from "@/../public/Avatar.png";
 
-import { useState } from "react";
-
-export default function Home() {
-
-  const [house, setHouse] = useState(0);
-
-  const addHouse = function() {
-    setHouse(house + 1);
-  }
-
-  const handleAnimationComplete = () => {
-    console.log('Animation completed!');
-  };
-
+export default function About() {
   return (
-    <div className="flex w-full h-full bg-red justify-center items-center rounded-2xl">
-
-      <div className="cursor-pointer flex flex-col justify-center items-center"
-        onClick={addHouse}
-      >
-        <div className="text-6xl">{'🏚️'}</div>  {/* Broken house emoji */}
-        <div className="text-2xl">{house}</div>
-      </div>
+    <div className="w-full h-full bg-white flex justify-center items-center rounded-2xl">
       
+      <p>HOME</p>
     </div>
   );
 }
-
